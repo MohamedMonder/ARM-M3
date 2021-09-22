7172663615294c4cabe104022191e8c4ed933730
Mohamed Monder Mohamed <mohamedmonder@icloud.com>
2021-09-22 15:24:11 +0200

    Add files via upload

---
+++ 01-MCAL/02-STK/STK_config.h
@@ -0,0 +1,13 @@
+/*****************************************/
+/* Author  :  Ahmed Monder               */
+/* Version :  V01                        */
+/*****************************************/
+#ifndef STK_CONFIG_H
+#define STK_CONFIG_H
+
+
+/* Options: MSTK_SRC_AHB / MSTK_SRC_AHB_8 */
+#define MSTK_CLK_SRC     MSTK_SRC_AHB_8
+
+
+#endif
